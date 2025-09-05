a23358fb159ecd75cab07603256fb5f35391151e
Greta <asth0010@elev.techcollege.dk>
2025-09-05 11:24:20 +0200

    Data layer done

--- Javascript/View/Script.js
+++ Javascript/View/Script.js
@@ -1,38 +1,130 @@
 // HER LÆGGER VI ALLE JAVASCRIPT FILES SAMMEN
 
-// VARIABLE
-let mainContainer =document.getElementById("mainContainer")
-let objData = getData()
-let obj = {}
+// #region GLOBALS
+const contentSection = document.getElementById("mainContainer");
+let currentData=null;
+// #endregion
 
-// CREATE BUTTON
-let createButton = document.createElement("button")
-createButton.innerText = 'create new'
+initApp()
+
+// #region MODEL CODE
+function getData(){
+    console.log('getData');
+
+    return JSON.parse(localStorage.getItem("toDoApp_v1"));
+}
+function saveData(myData){
+    console.log('saveData');
+    let serializesData=JSON.stringify(myData) // ingen gåseben, det er en functions parameter
+
+    localStorage.getItem("toDoApp_v1", serializesData);
+}
+function makeNewData(){
+    console.log('makeNewData');
+    //DUMMY DATA, HUSK AT TØMME LISTER INDEN DEPLOYMENT
+    let newData={
+        darkMode:false,
+        lists:[ //dataobject
+            {
+                listName:"list 1",
+                items:[{name: "opvask", done:false},{name:"Vaske Tøj", done:true}, {name:"Vaske gulv", done:false}]
+            },
+
+             {
+                listName:"list 1",
+                items:[{name: "Kød", done:false},{name:"Salat", done:true}, {name:"Sovs", done:false}]
+            }
+        ]
+
+    }
+    
+    //RETURN NEWDATA
+    return newData;
+}
+
+// #endregion
+
+// #region CONTROLLER CODE , (den skal skrives først)
+function initApp(){
+    console.log('initApp');
+    
+    // hent data
+    currentData=getData();
+    if (currentData==null) {
+        //VI HAR IKKE DATA
+
+        currentData = makeNewData()
+        saveData(currentData)
+        //VIS DATA TIL USER
+        
+    } 
+        // VIS DATA TIL USER
+        
+
+
+
+    
+    // evaluer data
+
+}
+// #endregion
+// #region VIEW CODE
+// #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // GLOBAL VARIABLE
+// let mainContainer =document.getElementById("mainContainer")
+// let objData = getData()
+// // let obj = {}
+
+// // CREATE BUTTON
+// let createButton = document.createElement("button")
+// createButton.innerText = 'create new'
 
 // createButton.addEventListener("click",createInput)
-mainContainer.appendChild(createButton)
+// mainContainer.appendChild(createButton)
 
-// SAVE BUTTON
-let saveButton = document.createElement("button")
-saveButton.innerText ='Save'
+// // SAVE BUTTON
+// let saveButton = document.createElement("button")
+// saveButton.innerText ='Save'
 // saveButton.addEventListener("click", saveData)
-mainContainer.appendChild(saveButton)
-console.log (saveButton)
+// mainContainer.appendChild(saveButton)
+// console.log (saveButton)
 
-// LOAD VORES DATA
-function getData(){
-    let data = localStorage.getItem('husk')
-    let parsedData =JSON.parse(data)
-    return parsedData
-}
-function createInput (obj_data){
+// // LOAD VORES DATA
+// function getData(){
+//     let data = localStorage.getItem('husk')
+//     let parsedData =JSON.parse(data)
+//     return parsedData
+// }
+// function createInput (obj_data){
 
-    let inputElement = document.createElement("Input")
-    if(obj_data){
-        inputElement.id = obj_data.id
-        inputElement.value = obj_data.value
-    }
-    else{
-        inputElement.id = crypto.randomUUID()
-    }
-}
+//     let inputElement = document.createElement("Input")
+//     if(obj_data){
+//         inputElement.id = obj_data.id
+//         inputElement.value = obj_data.value
+//     }
+//     else{
+//         inputElement.id = crypto.randomUUID()
+//     }
+// }
+// function saveData(){
+//     let data = localStorage.saveItem('husk')
+//     let parsedData =JSON.parse(data)
+//     return parsedData
+// }
